9b3ef0926c03bc2fe138f35eec92452b59aa2a5a
eugeneChoi <chyj2108@naver.com>
2024-08-07 15:46:04 +0900

    feat: 플랜 구현

--- j-front/src/App.js
+++ j-front/src/App.js
@@ -1,42 +1,41 @@
 import './App.css';
-import React, {useState, useEffect, Suspense, lazy} from 'react';
-import {
-  BrowserRouter as Router, Navigate,
-  Route,
-  Routes
-} from 'react-router-dom';
+import React, { useState, Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import Header from "./component/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from "./component/SignUp";
 import Post from "./pages/Post";
 import Posting from "./pages/Posting";
 import ProtectedRoute from "./context/ProtectedRoute";
+import Plan from './component/Plan'; // Plan 컴포넌트 임포트
 
 const Login = lazy(() => import('./component/Login'));
 const Home = lazy(() => import('./pages/Home'));
 
 function App() {
-  const [showSignUp, setShowSignUp] = useState(true);
+    const [showSignUp, setShowSignUp] = useState(true);
 
-  // Define the toggle function
-  const handleToggle = () => {
-    setShowSignUp(!showSignUp);
-  };
-  return (
-      <Router>
-        <Header />
-        <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/home" element={<ProtectedRoute component={Home} />} />
-          <Route path="/login" element={<Login toggle={handleToggle} />} />
-          <Route path="/signup" element={<SignUp toggle={handleToggle} />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/posting" element={<Posting />} />
-          <Route path="/" element={<Navigate to="/home" />} />
-        </Routes>
-        </Suspense>
-      </Router>
-  );
+    // Define the toggle function
+    const handleToggle = () => {
+        setShowSignUp(!showSignUp);
+    };
+
+    return (
+        <Router>
+            <Header />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/home" element={<ProtectedRoute component={Home} />} />
+                    <Route path="/login" element={<Login toggle={handleToggle} />} />
+                    <Route path="/signup" element={<SignUp toggle={handleToggle} />} />
+                    <Route path="/post" element={<Post />} />
+                    <Route path="/posting" element={<Posting />} />
+                    <Route path="/plan" element={<Plan />} />
+                    <Route path="/" element={<Navigate to="/home" />} />
+                </Routes>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
